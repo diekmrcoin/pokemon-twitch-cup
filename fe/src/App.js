9b41e3933185c4fc58c21308f1e20f830952b40c
Diego Maroto <diego.diemg@gmail.com>
2022-10-27 16:02:30 +0200

    Added some useful links to the main page

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
-import { FaTwitter } from "react-icons/fa";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FaTwitter, FaGithub } from "react-icons/fa";
 import team from "./img/team.compressed.jpeg";
 import "./App.css";
 
@@ -8,21 +10,40 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <div>
-          <p className="contact_me">
-            Habla conmigo!{" "}
-            <a
-              href="https://twitter.com/diekmrcoin"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Button>
-                <FaTwitter /> @diekmrcoin
-              </Button>
-            </a>
-          </p>
-        </div>
-        <br />
+        <Row>
+          <Col>
+            <p className="contact_me">
+              Habla conmigo!{" "}
+              <a
+                href="https://twitter.com/diekmrcoin"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button>
+                  <FaTwitter /> @diekmrcoin
+                </Button>
+              </a>
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="colaborate">
+              Éste proyecto es libre y de código abierto, siéntete libre de
+              proponer, revisar, programar o comentar.{" "}
+              <a
+                href="https://github.com/diekmrcoin/pokemon-twitch-cup"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button variant="secondary">
+                  <FaGithub /> PokeTwitchCup
+                </Button>
+              </a>
+            </p>
+          </Col>
+        </Row>
+        <hr />
         <div>
           <img
             className="appTeamImage"
